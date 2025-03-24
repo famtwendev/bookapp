800d1e3e1430db01b9558af403c033d8856b5ce1
Famtwen <phamtuyen121314@gmail.com>
2025-03-24 14:52:59 +0700

    #6 Authorization trong microservices với JWT

---
+++ api-gateway/src/main/java/com/famtwen/gateway/dtos/ApiResponse.java
@@ -0,0 +1,25 @@
+package com.famtwen.gateway.dtos;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.ZonedDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+    @Builder.Default
+    int code = 1000;
+
+    String message;
+    T result;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "Asia/Ho_Chi_Minh")
+    final ZonedDateTime timestamp = ZonedDateTime.now();
+}
